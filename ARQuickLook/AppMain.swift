0ffa99a6d33aafebd6918cef291ddc0bb44c9169
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-23 21:52:02 +0100

    finish set up

---
+++ ARQuickLook/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  ARQuickLook
+//
+//  Created by Zaid Neurothrone on 2022-12-23.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
