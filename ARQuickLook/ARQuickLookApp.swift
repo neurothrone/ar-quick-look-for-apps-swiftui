85d0f2654dd670fc07850cce3927fa6fd492de96
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-23 21:50:42 +0100

    Initial Commit

---
+++ ARQuickLook/ARQuickLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARQuickLookApp.swift
+//  ARQuickLook
+//
+//  Created by Zaid Neurothrone on 2022-12-23.
+//
+
+import SwiftUI
+
+@main
+struct ARQuickLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
